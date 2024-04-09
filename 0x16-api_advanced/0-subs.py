c6788221403ba2f0ecabfafec7dea2287bbf2a31
Eunice-Serani <eaamable91@gmail.com>
2024-04-09 06:41:37 -0700

    Recurse and count it

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns number of subscribers
-for a given subreddit
+This implements the number_of_subscribers function
 """
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns number of subscribers
-    (not active users, total subscribers) for a given subreddit
-
-    Args:
-        subreddit (str): The subreddit to query subscribers for
-
-    Returns:
-        Total subscribers, otherwise 0
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Chrome 121"}
-
-    try:
-        r = requests.get(url, headers=headers, allow_redirects=False)
-        if r.status_code == 200:
-            data = r.json()
-            return data['data']['subscribers']
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    except Exception as e:
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
